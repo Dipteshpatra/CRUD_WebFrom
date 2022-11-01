b0a1e4a3fe46f444ed5861e0a5ed1a5b1e5f1e3a
Dipteshpatra <113527751+Dipteshpatra@users.noreply.github.com>
2022-11-01 14:10:08 +0530

    Add files via upload
    performed crud op using webfrom

---
+++ CRUD_OP_WEBFROM/CRUD_OP.aspx
@@ -0,0 +1,31 @@
+﻿
+
+<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CRUD_OP.aspx.cs" Inherits="CRUD_OP_WEBFROM.CRUD_OP" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+             <table>
+                <tr><td> Student Name:<asp:TextBox ID="StdName" runat="server"></asp:TextBox></td></tr>
+                <tr> <td> Student Class:<asp:TextBox ID="StdCls" runat="server"></asp:TextBox></td></tr>
+                <tr><td>Student Mob<asp:TextBox ID="StdMno" runat="server"></asp:TextBox></td></tr>
+                  <tr><td>Student Stream<asp:TextBox ID="StdStrm" runat="server"></asp:TextBox></td></tr>
+                <tr>
+                    <td><asp:Button ID="insert" runat="server" Text="insert" OnClick="add_Click" /></td>
+                   <td><asp:Button ID="Update" runat="server" Text="Update" OnClick="Update_Click" /></td>
+                   <td><asp:Button ID="Read" runat="server" Text="Read" OnClick="Read_Click" /></td>
+                   <td><asp:Button ID="Delete" runat="server" Text="Delete" OnClick="Delete_Click" /></td>
+                    <asp:GridView ID="Display" runat="server"></asp:GridView>
+                 </tr>
+                <tr><td><asp:Label ID="message" runat="server" ></asp:Label></td></tr>
+</table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ CRUD_OP_WEBFROM/CRUD_OP.aspx.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.Services.Description;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CRUD_OP_WEBFROM
+{
+    public partial class CRUD_OP : System.Web.UI.Page
+    { 
+
+         SqlConnection sqlConnection = new SqlConnection(@"Data Source =.; Initial Catalog = StudentManagment; Integrated Security = True");
+    protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void add_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("InsertStudent", sqlConnection);
+            //craete crud
+              cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@StdName", SqlDbType.VarChar, 50).Value = StdName.Text.ToString();
+            cmd.Parameters.Add("@StdCls", SqlDbType.VarChar, 50).Value = StdCls.Text.ToString();
+            cmd.Parameters.Add("@StdMno", SqlDbType.BigInt).Value =Int64.Parse(StdMno.Text);
+            cmd.Parameters.Add("@StdStrm", SqlDbType.VarChar, 50).Value = StdStrm.Text.ToString();
+            try
+            {
+                sqlConnection.Open();
+                cmd.ExecuteNonQuery();
+                message.Text = "Data Updated";
+            }
+
+            catch(Exception ex) 
+            {
+                throw ex;
+            }
+            finally
+            {
+                sqlConnection.Close(); 
+            }
+        }
+
+        protected void Update_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                sqlConnection.Open();
+                string updatequery = "EXEC SP_UpdateStudentInfo  '" + StdName.Text + "' , '" + StdCls.Text + "','" + StdMno.Text + "','" + StdStrm.Text + "' ";
+                SqlCommand Command = new SqlCommand(updatequery, sqlConnection);
+                Command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
+
+        protected void Read_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                sqlConnection.Open();
+                string displayquery = "select*from SP_DisPlayStudentInfo";
+                SqlCommand Command = new SqlCommand(displayquery, sqlConnection);
+                Command.ExecuteNonQuery();
+                DataTable dt = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(Command);
+                da.Fill(dt);
+                Display.DataSource = dt;
+                Display.DataBind();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            { sqlConnection.Close(); }
+        }
+
+        protected void Delete_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                sqlConnection.Open();
+                string deletequery = "Exec SP_DeleteStudentinfo '" + StdName.Text + "' ";
+                SqlCommand cmd = new SqlCommand(deletequery, sqlConnection);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+
+        }
+    }
+}
